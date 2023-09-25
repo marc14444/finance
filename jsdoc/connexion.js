5b4fec6c1803f67dbe08ec28c69e2f4efd7763f4
Marc Harden <Marc_Harden@192.168.50.69>
2023-09-25 15:33:56 +0000

    15e commit

--- jsdoc/connexion.js
+++ jsdoc/connexion.js
@@ -1,24 +1,59 @@
+const admin = [
+    {
+        email: "amanijeanmarc41@gmail.com",
+        motDePasse: "imbecile1"
+    }
+]
+localStorage.setItem("dataAdmin", JSON.stringify(admin))
+
 let boutton = document.querySelector("#connexion")
 let formulaire = document.querySelector("#formulaire")
 formulaire .addEventListener("submit", (e) =>{
     e.preventDefault();
     let userlist = JSON.parse(localStorage.getItem("tab_LS"))
+    let dataAdmin = JSON.parse(localStorage.getItem("dataAdmin"))
     let result ;
+    let verifAdmin;
+    // let verifAdmin = dataAdmin.find(emailInput => emailInput.value === dataAdmin.email && pwdInput.value === dataAdmin.motDePasse)
+
+
+    // RECHERCHE  ET VERIF DE ADMIN 
+    dataAdmin.forEach((admin) => {
+        if (document.querySelector("#adresseMail").value === admin.email && document.querySelector("#motDePasse").value === admin.motDePasse){
+            verifAdmin = admin
+            console.log(verifAdmin);
+        }
+    });
+    // RECHERCHE  ET VERIF DE USER 
     userlist.forEach((user) => {
         if (document.querySelector("#adresseMail").value == user.adresseMail && document.querySelector("#motDePasse").value === user.motDePasse){
             result = user
             console.log(result);
         }
     });
-    if (typeof result !== "undefined"){
+
+    if (typeof verifAdmin === 'object') {
+        window.location.replace("./../htmldoc/dashboard.html");
+    } else if (typeof result === "object") {
         localStorage.setItem("session", JSON.stringify(result))
         window.location.replace("./../htmldoc/connecter.html");
-    }
-    else{
+    }else{
         alert("introuvable")
-         window.location.reload()
-    }
+        window.location.reload()
+    } 
 
-     userlist.push(user)
-    localStorage.setItem("tab_LS", JSON.stringify(userlist));
+
+
+    // console.log(typeof verifAdmin);
+    // if (typeof verifAdmin != 'undefined') {
+    //     window.location.replace("./../htmldoc/dashboard.html");
+    // }
+
+    // if (typeof result !== "undefined"){
+    //     localStorage.setItem("session", JSON.stringify(result))
+    //     window.location.replace("./../htmldoc/connecter.html");
+    // }
+
+    //  userlist.push(user)
+    // localStorage.setItem("tab_LS", JSON.stringify(userlist));
 })
